9338e9b227db01da0f308adc419118ba9d23870c
Valynor <2079654+Valinor@users.noreply.github.com>
2024-12-04 15:59:26 +0100

    Typo correction day01

--- valynor/day04/main.py
+++ valynor/day04/main.py
@@ -1,3 +1,5 @@
+import itertools
+
 sample="""MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
@@ -19,41 +21,37 @@ def parse_data(data):
 
 def check_in_direction_from(data,x,y,directionx,directiony,current_objective):
     for i in range(0,len(current_objective)):
-        if x+i*directionx<0 or x+i*directionx>=len(data):
-            return False
-        if y+i*directiony<0 or y+i*directiony>=len(data[0]):
-            return False
-        if data[x+i*directionx][y+i*directiony]!=current_objective[i]:
+        if x+i*directionx<0 or x+i*directionx>=len(data) or\
+            y+i*directiony<0 or y+i*directiony>=len(data[0]) or \
+            data[x + i * directionx][y + i * directiony] != current_objective[i]:
             return False
     return True
 
 def check_in_direction(data,directionx,directiony,current_objective):
-    total=0
-    for x in range(0,len(data)):
-        for y in range(0,len(data[0])):
-            if data[x][y]==current_objective[0]:
-                total+=check_in_direction_from(data,x,y,directionx,directiony,current_objective)
-    return total
+    return sum(check_in_direction_from(data, x, y, directionx, directiony, current_objective)
+               for (x,y) in list(itertools.product(range(0,len(data)), range(0,len(data[0]))))
+               if data[x][y]==current_objective[0])
 
 def check_all_directions(data,current_objective):
-    total=0
-    for x in range(-1,2):
-        for y in range(-1,2):
-            if x!=0 or y!=0:
-                total+=check_in_direction(data,x,y,current_objective)
-    return total
+    combinaison=list(itertools.product(range(-1,2), range(-1,2)))
+    combinaison.remove((0,0))
+    return sum(check_in_direction(data,x,y,current_objective)
+        for x,y in combinaison)
 
 def checkMAS(data):
-    total=0
-    for x in range(1,len(data)-1):
-        for y in range(1,len(data[0])-1):
-            if data[x][y]=="A" and ((data[x-1][y-1]=="M" and data[x+1][y+1]=="S") or (data[x-1][y-1]=="S" and data[x+1][y+1]=="M")) \
-               and ((data[x+1][y-1]=="M" and data[x-1][y+1]=="S") or (data[x+1][y-1]=="S" and data[x-1][y+1]=="M")):
-                   total=total+1
-    return total
+    return sum(
+        [
+            data[x][y]=="A" and
+            ((data[x-1][y-1]=="M" and data[x+1][y+1]=="S") or
+             (data[x-1][y-1]=="S" and data[x+1][y+1]=="M")) and
+            ((data[x+1][y-1]=="M" and data[x-1][y+1]=="S") or
+             (data[x+1][y-1]=="S" and data[x-1][y+1]=="M"))
+            for x,y in list(itertools.product( range(1,len(data)-1), range(1,len(data[0])-1)))
+        ]
+    )
 
 assert(check_all_directions(parse_data(sample),objective)==18)
-print(f"Part 1 result : {checkMAS(parse_data(sample))}")
+print(f"Part 1 result : {check_all_directions(parse_data(input_data),objective)}")
 
 assert(checkMAS(parse_data(sample))==9)
 print(f"Part 2 result : {checkMAS(parse_data(input_data))}")
